9a0069aaf19071313df50ed799ced33b5c1d67e6
whdbcks111 <81742754+whdbcks111@users.noreply.github.com>
2022-09-16 10:30:05 +0900

    slide bug fixed

--- Assets/Scripts/Util/UIHoverSlide.cs
+++ Assets/Scripts/Util/UIHoverSlide.cs
@@ -1,25 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class UIHoverSlide : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField]
     private Vector2 _slideAxis;
+    [SerializeField]
+    private RectTransform _rectTransform;
 
     private Vector2 _startPos;
     private bool _hover = false;
 
     private void Start() 
     {
-        _startPos = transform.position;
+        _startPos = _rectTransform.anchoredPosition;
         Time.timeScale = 1f;
     }
 
     private void Update()
     {
-        transform.position = Vector2.Lerp(transform.position, 
+        _rectTransform.anchoredPosition = Vector2.Lerp(_rectTransform.anchoredPosition, 
                 _hover ? _startPos + _slideAxis : _startPos, Time.deltaTime * 10);
     }
 
